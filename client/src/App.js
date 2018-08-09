1bcaad9029804e46f787f3f26bb372f4671750c6
neenu <neenukurian92@gmail.com>
2018-08-09 12:37:46 +0200

    aded components,reducers,actions

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
+//components
+import Header from './components/Header';
+import TopTracks from './components/TopTracks/TopTracks'
+import Recommendations from './components/recommendations/Recommendations'
+//action creators
+import { fetchUser,fetchTopTracks,fetchRecommendations} from './actions/userActions';
+import { setToken } from './actions/tokenActions';
+//css
 import './App.css';
 
 class App extends Component {
+
+  //set access token once component is mounted
+  componentDidMount(){
+
+    let hashParams = {};
+	  let e, r = /([^&;=]+)=?([^&;]*)/g,
+	    q = window.location.hash.substring(1);
+	  while ( (e = r.exec(q))!=null) {
+	    hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    
+    if(!hashParams.access_token) {
+	    window.location.href = 'https://accounts.spotify.com/authorize?client_id=a417fb58f6c84d0e83555b8137882a97&scope=user-read-email%20user-top-read%20user-read-private&response_type=token&redirect_uri=http://localhost:3000/callback';
+	  } else {
+	    this.props.setToken(hashParams.access_token);
+	  }
+   
+  }
+  
+  //fetches user details, top tracks and recommendations
+  componentWillReceiveProps(nextProps) {
+
+	  if(nextProps.token) {
+      this.props.fetchUser(nextProps.token);
+      this.props.fetchTopTracks(nextProps.token);
+      this.props.fetchRecommendations(nextProps.token)
+    };
+  }
+
   render() {
+      
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+  
+        <div >
+        <Header/>
+        <TopTracks/>
+        <Recommendations/>
+        </div>
+     
+      
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+
+  return {
+    token: state.tokenReducer.token
+  };
+
+};
+
+
+
+export default connect(mapStateToProps, {setToken,fetchUser,fetchTopTracks,fetchRecommendations})(App);
